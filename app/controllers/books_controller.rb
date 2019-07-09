1ba568bfbf7028bf70016a13893f72b07cb97a29
岡本憲聖 <okamotokensei@okamotokenshounoMacBook-puro.local>
2019-07-09 14:16:52 +0900

    [Start]top-index view

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+
+  def show
+  end
+
+	def index
+		@book = Book.new
+		@books = Book.all
+	end
+
+	def create
+		book = Book.new(book_params)
+		book.save
+		redirect_to books_path
+	end
+
+  def edit
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
+end
